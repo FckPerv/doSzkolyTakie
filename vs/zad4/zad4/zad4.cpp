55e9e0b5737e00ea2345e94bc21745b4cedf3800
FckPerv <maksymilkk@gmail.com>
2022-10-19 15:35:20 +0200

    zad 4.1

---
+++ vs/zad4/zad4/zad4.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int silniaIteracyjna(int n)
+{
+    int odp=1;
+    for (int i = 1; i <= n; i++)
+    {
+        odp *= i;
+    }
+    return odp;
+}
+
+int silniaRekurenkcyjna(int n)
+{
+    int odp = n;
+    if (n > 1)
+    {
+        odp *= silniaRekurenkcyjna(n - 1);
+    }
+    return odp;
+    
+    
+    
+}
+int main()
+{
+    cout << silniaIteracyjna(1)<< endl;
+    cout << silniaRekurenkcyjna(5) << endl;
+}
+
