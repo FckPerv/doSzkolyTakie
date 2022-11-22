1ae33c2dc0271871524b3d5afc36322fc5cd23da
FckPerv <maksymilkk@gmail.com>
2022-11-22 15:03:27 +0100

    do 6.6

--- vs/zad6/zad6/zad6.cpp
+++ vs/zad6/zad6/zad6.cpp
@@ -1,20 +1,94 @@
-﻿// zad6.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿
 #include <iostream>
+using namespace std;
+
+#define N 40
+
+void losowanieTablicy(int tab[], int min, int max)
+{
+    
+    for (int i = 0; i < N ; i++)
+    {
+        int a = rand() % (max - min+1) + min;
+        tab[i] = a;
+    }
+}
+
+void wypisz(int tab[])
+{
+    cout << " elementy tablicy: " << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cout << " " << tab[i] << " ";
+    }
+    cout << endl;
+}
+
+void sumaKwadratow(int tab[])
+{
+    int suma = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (tab[i] % 2 == 1)
+        {
+            suma += tab[i] * tab[i];
+        }
+    }
+    cout << "suma kwadratow elementow nie parzystych tej tablicy wynosi: " << suma << endl;
+}
+
+void znajdzMax(int tab[], int* ind, int& war)
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        if (tab[i] >= war)
+        {
+            war = tab[i];
+            *ind = i;
+        }
+    }
+}
+
+void odchSrd(int tab[])
+{
+    float srednia = 0;
+    for (int i = 0; i < N; i++)
+    {
+        srednia += tab[i];
+    }
+    srednia /= N;
+
+    float odchylenie = 0;
+    for (int i = 0; i < N; i++)
+    {
+        odchylenie += sqrt((srednia - tab[i])*(srednia - tab[i]));
+    }
+
+    cout << "srednie odchylenie tej tablicy wynosi: " << odchylenie << endl;
+}
+
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int Y[N];
+    int X[N];
+    int war = 0;
+    int ind = 0;
+    losowanieTablicy(X,8,20);
+    losowanieTablicy(Y,8,20);
+    cout << "tablica X" << endl;
+    wypisz(X);
+    cout << "tablica Y" << endl;
+    wypisz(Y);
+    sumaKwadratow(X);
+    znajdzMax(Y,&ind, war);
+    cout <<"index: " << ind << "wartosc: " << war;
+
+
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
