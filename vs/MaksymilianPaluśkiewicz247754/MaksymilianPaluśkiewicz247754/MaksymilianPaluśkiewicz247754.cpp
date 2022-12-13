2c03e854991bfa4fb8ce3d8fc218fb5a2999c845
FckPerv <maksymilkk@gmail.com>
2022-12-13 14:11:15 +0100

    nwm nowe jakies

---
+++ vs/MaksymilianPaluśkiewicz247754/MaksymilianPaluśkiewicz247754/MaksymilianPaluśkiewicz247754.cpp
@@ -0,0 +1,68 @@
+﻿
+//Maksymilian Paluśkiewicz 247754
+#include <iostream>
+#include <iomanip>
+#define n 30
+using namespace std;
+
+double sinus(double x)
+{
+    return 20*sin(x / 3) + 22;
+}
+
+void wyswietl(double tab[n])
+{
+    cout << "zawartosc tablicy: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        if (tab[i] >= 10)
+        {
+            cout << setprecision(4) << tab[i] << setw(7);
+        }
+        else
+        {
+            cout << setprecision(3) << tab[i] << setw(7);
+        }
+        
+    }
+    cout << endl;
+}
+
+void srednia(double tab[n], double* wynik)
+{
+    double srednia = 0;
+    for (int i = 0; i < n; i++)
+    {
+        srednia += tab[i];
+    }
+    srednia = srednia / n;
+    *wynik = srednia;
+}
+
+void rysuj(double tab[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < floor(tab[i]); j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    double tab[n];
+    double odp5 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        tab[i] = sinus(i);
+    }
+    wyswietl(tab);
+    srednia(tab, &odp5);
+    cout << "srednia tablicy: " << odp5 << endl;
+    rysuj(tab);
+
+}
+
+
---
+++ vs/k1/k1/k1.cpp
@@ -0,0 +1,52 @@
+﻿
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+#define n 20
+
+double pierwiastek4(float a)
+{
+    return sqrt(sqrt(a));
+}
+
+void uzupelnij(double tab[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        tab[i] = pierwiastek4(i+1);
+    }
+}
+
+void wyswietl(double tab[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << setw(10) << i + 1 << setw(10) << tab[i] << setw(10) << endl;
+    }
+}
+
+void suma1(double& suma, double tab[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        suma += tab[i];
+    }
+}
+
+
+int main()
+{
+    double tab[n] = {};
+    uzupelnij(tab);
+    wyswietl(tab);
+    double *wskaznik;
+    wskaznik = &tab[n - 1];
+    cout << *wskaznik;
+    double suma=0;
+    suma1(suma, tab);
+    cout << endl<<suma<<endl;
+
+}
+
+
